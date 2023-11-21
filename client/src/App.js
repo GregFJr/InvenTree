9cd80a931e90849f2a41691e50ff157c4acc079a
GregFJr <finney101690@gmail.com>
2023-11-21 12:16:27 -0700

    created splashacreen and logic for fade-in nad fae-out redirect

--- client/src/App.js
+++ client/src/App.js
@@ -2,11 +2,13 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import AddProductForm from "./components/AddProductForm";
 import DisplayAllProducts from "./components/DisplayAllProducts";
+import SplashScreen from "./components/SplashScreen";
 
 function App() {
   return (
     <Router>
       <Routes>
+        <Route path="/" element={<SplashScreen />} />
         <Route path="/home" element={<AddProductForm />} />
         <Route path="/products" element={<DisplayAllProducts />} />
       </Routes>
---
+++ client/src/components/SplashScreen.jsx
@@ -0,0 +1,51 @@
+import React, { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import '../SplashScreen.css';
+
+const SplashScreen = () => {
+  const [fadeProp, setFadeProp] = useState('fade-in');
+  let navigate = useNavigate();
+
+  useEffect(() => {
+    const fadeOutTimeout = setTimeout(() => {
+      setFadeProp('fade-out');
+    }, 3000); 
+
+    const navigateTimeout = setTimeout(() => {
+      navigate('/home'); 
+    }, 6000); 
+
+    return () => {
+      clearTimeout(fadeOutTimeout);
+      clearTimeout(navigateTimeout);
+    };
+  }, [navigate]);
+
+  const handleImageSizing = () => {
+    const aspectRatio = window.innerWidth / window.innerHeight;
+    return {
+      ...splashStyle,
+      backgroundSize: aspectRatio > 1 ? '100% 100%' : 'cover',
+    };
+  };
+
+  const splashStyle = {
+    height: '100vh',
+    width: '100vw',
+    backgroundImage: 'url(/splash.png)',
+    backgroundPosition: 'center center',
+    backgroundRepeat: 'no-repeat',
+    transition: 'opacity 3s ease-in-out',
+  };
+
+  return (
+    <div
+      className={`splash-screen ${fadeProp}`}
+      style={handleImageSizing()}
+    />
+  );
+};
+
+export default SplashScreen;
+
+
