1db0e83b220104bc4b1a8aa5759c0dce1fcc46cf
GregFJr <finney101690@gmail.com>
2023-11-22 12:40:55 -0700

    updated delete resolver

--- client/src/components/DisplayAllProducts.jsx
+++ client/src/components/DisplayAllProducts.jsx
@@ -1,8 +1,9 @@
 import React, { useState, useEffect } from 'react';
-import { useQuery, gql } from '@apollo/client';
+import { useQuery, useMutation, gql } from '@apollo/client';
 import '../styles/DisplayAllProducts.css';
 import Navbar from './NavBar';
 
+// GraphQL queries and mutations
 const GET_PRODUCTS_BY_CATEGORY = gql`
   query {
     products {
@@ -16,10 +17,20 @@ const GET_PRODUCTS_BY_CATEGORY = gql`
   }
 `;
 
+const DELETE_PRODUCT_MUTATION = gql`
+  mutation DeleteProduct($id: ID!) {
+    deleteProduct(id: $id) {
+      id
+    }
+  }
+`;
+
 const ProductsByCategory = () => {
-  const { loading, error, data } = useQuery(GET_PRODUCTS_BY_CATEGORY);
+  const { loading, error, data, refetch } = useQuery(GET_PRODUCTS_BY_CATEGORY);
+  const [deleteProduct] = useMutation(DELETE_PRODUCT_MUTATION);
   const [productsByCategory, setProductsByCategory] = useState({});
 
+  // Effect to group products by category
   useEffect(() => {
     if (data && data.products) {
       const groupedByCategory = data.products.reduce((acc, product) => {
@@ -28,10 +39,32 @@ const ProductsByCategory = () => {
         acc[category].push(product);
         return acc;
       }, {});
-
       setProductsByCategory(groupedByCategory);
     }
   }, [data]);
+
+  // Handle product deletion
+  const handleDelete = (productId) => {
+    if (window.confirm('Are you sure you want to delete this product?')) {
+      deleteProduct({
+        variables: { id: productId },
+        update: (cache) => {
+          // Get current products in cache
+          const existingProducts = cache.readQuery({ query: GET_PRODUCTS_BY_CATEGORY });
+          // Filter out the product we are deleting
+          const newProducts = existingProducts.products.filter((product) => product.id !== productId);
+          // Write the new products to the cache
+          cache.writeQuery({
+            query: GET_PRODUCTS_BY_CATEGORY,
+            data: { products: newProducts },
+          });
+        },
+      }).then(() => {
+        // Optionally refetch products after deletion for instant UI update
+        refetch();
+      });
+    }
+  };
 
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error :(</p>;
@@ -40,14 +73,17 @@ const ProductsByCategory = () => {
     <div>
       <Navbar />
       {Object.keys(productsByCategory).map((category) => (
-        <div className='cat-container' key={category}>
-          <h2 className='cat-heading'>{category}</h2>
+        <div className="cat-container" key={category}>
+          <h2 className="cat-heading">{category}</h2>
           {productsByCategory[category].map((product) => (
-            <div key={product.id}>
-              <h3 className='prod-name'>{product.name}</h3>
-              <p className='prod-info'>{product.description}</p>
-              <p className='prod-info'>Price: ${product.price}</p>
-              <p className='prod-info'>Quantity: {product.quantity}</p>
+            <div key={product.id} className="product-item">
+              <h3 className="prod-name">{product.name}</h3>
+              <p className="prod-info">{product.description}</p>
+              <p className="prod-info">Price: ${product.price}</p>
+              <p className="prod-info">Quantity: {product.quantity}</p>
+              <button onClick={() => handleDelete(product.id)} className="btn btn-danger btn-sm">
+                Delete
+              </button>
             </div>
           ))}
         </div>
@@ -57,3 +93,4 @@ const ProductsByCategory = () => {
 };
 
 export default ProductsByCategory;
+
--- server/resolvers/resolvers.js
+++ server/resolvers/resolvers.js
@@ -1,65 +1,78 @@
-const Product = require('../models/Product');
+const Product = require("../models/Product");
 
 const resolvers = {
-    Query: {
-      // Fetch all products
-      products: async () => {
-        try {
-          const products = await Product.find();
-          return products;
-        } catch (error) {
-          console.error(error);
-          throw new Error('Error fetching products');
-        }
-      },
-      // Fetch a single product by ID
-      product: async (_, { id }) => {
-        try {
-          const product = await Product.findById(id);
-          return product;
-        } catch (error) {
-          console.error(error);
-          throw new Error('Error fetching product');
-        }
-      },
+  Query: {
+    // Fetch all products
+    products: async () => {
+      try {
+        const products = await Product.find();
+        return products;
+      } catch (error) {
+        console.error(error);
+        throw new Error("Error fetching products");
+      }
     },
-    Mutation: {
-      // Add a new product
-      addProduct: async (_, { name, description, price, quantity, category }) => {
-        try {
-          const newProduct = new Product({ name, description, price, quantity, category });
-          const savedProduct = await newProduct.save();
-          return savedProduct;
-        } catch (error) {
-          console.error(error);
-          throw new Error('Error adding product');
-        }
-      },
-      // Update an existing product
-      updateProduct: async (_, { id, name, description, price, quantity, category }) => {
-        try {
-          const updatedProduct = await Product.findByIdAndUpdate(
-            id,
-            { name, description, price, quantity, category },
-            { new: true }
-          );
-          return updatedProduct;
-        } catch (error) {
-          console.error(error);
-          throw new Error('Error updating product');
-        }
-      },
-      // Delete a product
-      deleteProduct: async (_, { id }) => {
-        try {
-          await Product.findByIdAndDelete(id);
-          return true;
-        } catch (error) {
-          console.error(error);
-          return false;
-        }
-      },
+    // Fetch a single product by ID
+    product: async (_, { id }) => {
+      try {
+        const product = await Product.findById(id);
+        return product;
+      } catch (error) {
+        console.error(error);
+        throw new Error("Error fetching product");
+      }
     },
-  };
-  
-  module.exports = resolvers;
+  },
+  Mutation: {
+    // Add a new product
+    addProduct: async (_, { name, description, price, quantity, category }) => {
+      try {
+        const newProduct = new Product({
+          name,
+          description,
+          price,
+          quantity,
+          category,
+        });
+        const savedProduct = await newProduct.save();
+        return savedProduct;
+      } catch (error) {
+        console.error(error);
+        throw new Error("Error adding product");
+      }
+    },
+    // Update an existing product
+    updateProduct: async (
+      _,
+      { id, name, description, price, quantity, category }
+    ) => {
+      try {
+        const updatedProduct = await Product.findByIdAndUpdate(
+          id,
+          { name, description, price, quantity, category },
+          { new: true }
+        );
+        return updatedProduct;
+      } catch (error) {
+        console.error(error);
+        throw new Error("Error updating product");
+      }
+    },
+    // Delete a product
+    deleteProduct: async (_, { id }) => {
+      try {
+        const product = await Product.findById(id);
+        if (!product) {
+          throw new Error("Product not found");
+        }
+        await Product.findByIdAndDelete(id);
+        return product; 
+      } catch (error) {
+        console.error(error);
+        throw new Error("Error deleting product");
+      }
+    },
+  },
+};
+
+module.exports = resolvers;
