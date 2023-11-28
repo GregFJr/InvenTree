fbdd76ec0d9b4eea7f0a872c26d1ebf12c805644
GregFJr <finney101690@gmail.com>
2023-11-27 23:06:39 -0700

    updated middleware to serve static file from build folder for heroku

--- server/server.js
+++ server/server.js
@@ -23,10 +23,10 @@ async function startServer() {
 .catch(err => console.error('MongoDB connection error:', err));
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, 'client/build')));
+  app.use(express.static(path.join(__dirname, '../client/build')));
 
   app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+    res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
   });
 }
 
